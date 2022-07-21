2508ad0cd0b2938735b9a9da4f835d92734d3c28
DamianTEE <vladimirwayne9@gmail.com>
2022-07-21 04:38:51 +0100

    youthChurch

--- main.js
+++ main.js
@@ -1,18 +1,1 @@
-// Pause and play the video, and change the button text
 
-// Get the video
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
